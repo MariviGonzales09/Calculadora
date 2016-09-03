9a2cd64ec5dc9b8a99c3b9305b95e7df31b2c00c
Marivi Gonzales Almeyda <bailarcarola@hotmail.com>
2016-09-03 13:33:32 -0500

    Add files via upload

---
+++ Gonzales Almeyda/Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,362 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnCerrar = New System.Windows.Forms.Button()
+        Me.Txt1 = New System.Windows.Forms.TextBox()
+        Me.Btn7 = New System.Windows.Forms.Button()
+        Me.Btn8 = New System.Windows.Forms.Button()
+        Me.Btn9 = New System.Windows.Forms.Button()
+        Me.Btn4 = New System.Windows.Forms.Button()
+        Me.Btn5 = New System.Windows.Forms.Button()
+        Me.Btn6 = New System.Windows.Forms.Button()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.Btn2 = New System.Windows.Forms.Button()
+        Me.Btn3 = New System.Windows.Forms.Button()
+        Me.Btn0 = New System.Windows.Forms.Button()
+        Me.BtnBorrar = New System.Windows.Forms.Button()
+        Me.BtnSuma = New System.Windows.Forms.Button()
+        Me.BtnResta = New System.Windows.Forms.Button()
+        Me.BtnMultiplicar = New System.Windows.Forms.Button()
+        Me.Txt2 = New System.Windows.Forms.TextBox()
+        Me.BtnDividir = New System.Windows.Forms.Button()
+        Me.BtnPunto = New System.Windows.Forms.Button()
+        Me.BtnIgual = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Image = CType(resources.GetObject("Label1.Image"), System.Drawing.Image)
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 45)
+        Me.Label1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe Print", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(55, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 26)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Calculadora"
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnCerrar.ForeColor = System.Drawing.Color.DarkCyan
+        Me.BtnCerrar.Image = CType(resources.GetObject("BtnCerrar.Image"), System.Drawing.Image)
+        Me.BtnCerrar.Location = New System.Drawing.Point(285, 12)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.Size = New System.Drawing.Size(35, 35)
+        Me.BtnCerrar.TabIndex = 2
+        Me.BtnCerrar.UseVisualStyleBackColor = True
+        '
+        'Txt1
+        '
+        Me.Txt1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Txt1.Font = New System.Drawing.Font("Segoe Print", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt1.Location = New System.Drawing.Point(11, 61)
+        Me.Txt1.Multiline = True
+        Me.Txt1.Name = "Txt1"
+        Me.Txt1.Size = New System.Drawing.Size(309, 30)
+        Me.Txt1.TabIndex = 3
+        '
+        'Btn7
+        '
+        Me.Btn7.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn7.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn7.Location = New System.Drawing.Point(13, 127)
+        Me.Btn7.Name = "Btn7"
+        Me.Btn7.Size = New System.Drawing.Size(57, 50)
+        Me.Btn7.TabIndex = 4
+        Me.Btn7.Text = "7"
+        Me.Btn7.UseVisualStyleBackColor = False
+        '
+        'Btn8
+        '
+        Me.Btn8.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn8.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn8.Location = New System.Drawing.Point(76, 127)
+        Me.Btn8.Name = "Btn8"
+        Me.Btn8.Size = New System.Drawing.Size(57, 50)
+        Me.Btn8.TabIndex = 4
+        Me.Btn8.Text = "8"
+        Me.Btn8.UseVisualStyleBackColor = False
+        '
+        'Btn9
+        '
+        Me.Btn9.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn9.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn9.Location = New System.Drawing.Point(139, 127)
+        Me.Btn9.Name = "Btn9"
+        Me.Btn9.Size = New System.Drawing.Size(57, 50)
+        Me.Btn9.TabIndex = 4
+        Me.Btn9.Text = "9"
+        Me.Btn9.UseVisualStyleBackColor = False
+        '
+        'Btn4
+        '
+        Me.Btn4.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn4.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn4.Location = New System.Drawing.Point(12, 183)
+        Me.Btn4.Name = "Btn4"
+        Me.Btn4.Size = New System.Drawing.Size(57, 50)
+        Me.Btn4.TabIndex = 4
+        Me.Btn4.Text = "4"
+        Me.Btn4.UseVisualStyleBackColor = False
+        '
+        'Btn5
+        '
+        Me.Btn5.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn5.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn5.Location = New System.Drawing.Point(75, 183)
+        Me.Btn5.Name = "Btn5"
+        Me.Btn5.Size = New System.Drawing.Size(57, 50)
+        Me.Btn5.TabIndex = 4
+        Me.Btn5.Text = "5"
+        Me.Btn5.UseVisualStyleBackColor = False
+        '
+        'Btn6
+        '
+        Me.Btn6.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn6.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn6.Location = New System.Drawing.Point(138, 183)
+        Me.Btn6.Name = "Btn6"
+        Me.Btn6.Size = New System.Drawing.Size(57, 50)
+        Me.Btn6.TabIndex = 4
+        Me.Btn6.Text = "6"
+        Me.Btn6.UseVisualStyleBackColor = False
+        '
+        'Btn1
+        '
+        Me.Btn1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn1.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn1.Location = New System.Drawing.Point(12, 239)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(57, 50)
+        Me.Btn1.TabIndex = 4
+        Me.Btn1.Text = "1"
+        Me.Btn1.UseVisualStyleBackColor = False
+        '
+        'Btn2
+        '
+        Me.Btn2.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn2.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn2.Location = New System.Drawing.Point(75, 239)
+        Me.Btn2.Name = "Btn2"
+        Me.Btn2.Size = New System.Drawing.Size(57, 50)
+        Me.Btn2.TabIndex = 4
+        Me.Btn2.Text = "2"
+        Me.Btn2.UseVisualStyleBackColor = False
+        '
+        'Btn3
+        '
+        Me.Btn3.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn3.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn3.Location = New System.Drawing.Point(138, 239)
+        Me.Btn3.Name = "Btn3"
+        Me.Btn3.Size = New System.Drawing.Size(57, 50)
+        Me.Btn3.TabIndex = 4
+        Me.Btn3.Text = "3"
+        Me.Btn3.UseVisualStyleBackColor = False
+        '
+        'Btn0
+        '
+        Me.Btn0.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Btn0.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn0.Location = New System.Drawing.Point(12, 295)
+        Me.Btn0.Name = "Btn0"
+        Me.Btn0.Size = New System.Drawing.Size(184, 50)
+        Me.Btn0.TabIndex = 4
+        Me.Btn0.Text = "0"
+        Me.Btn0.UseVisualStyleBackColor = False
+        '
+        'BtnBorrar
+        '
+        Me.BtnBorrar.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnBorrar.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnBorrar.Location = New System.Drawing.Point(202, 127)
+        Me.BtnBorrar.Name = "BtnBorrar"
+        Me.BtnBorrar.Size = New System.Drawing.Size(118, 50)
+        Me.BtnBorrar.TabIndex = 4
+        Me.BtnBorrar.Text = "CR"
+        Me.BtnBorrar.UseVisualStyleBackColor = False
+        '
+        'BtnSuma
+        '
+        Me.BtnSuma.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnSuma.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnSuma.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnSuma.Location = New System.Drawing.Point(202, 183)
+        Me.BtnSuma.Name = "BtnSuma"
+        Me.BtnSuma.Size = New System.Drawing.Size(57, 50)
+        Me.BtnSuma.TabIndex = 4
+        Me.BtnSuma.Text = "+"
+        Me.BtnSuma.UseVisualStyleBackColor = False
+        '
+        'BtnResta
+        '
+        Me.BtnResta.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnResta.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnResta.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnResta.Location = New System.Drawing.Point(263, 183)
+        Me.BtnResta.Name = "BtnResta"
+        Me.BtnResta.Size = New System.Drawing.Size(57, 50)
+        Me.BtnResta.TabIndex = 4
+        Me.BtnResta.Text = "-"
+        Me.BtnResta.UseVisualStyleBackColor = False
+        '
+        'BtnMultiplicar
+        '
+        Me.BtnMultiplicar.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnMultiplicar.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnMultiplicar.Location = New System.Drawing.Point(202, 239)
+        Me.BtnMultiplicar.Name = "BtnMultiplicar"
+        Me.BtnMultiplicar.Size = New System.Drawing.Size(57, 50)
+        Me.BtnMultiplicar.TabIndex = 4
+        Me.BtnMultiplicar.Text = "*"
+        Me.BtnMultiplicar.UseVisualStyleBackColor = False
+        '
+        'Txt2
+        '
+        Me.Txt2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.Txt2.Font = New System.Drawing.Font("Segoe Print", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txt2.Location = New System.Drawing.Point(11, 89)
+        Me.Txt2.Multiline = True
+        Me.Txt2.Name = "Txt2"
+        Me.Txt2.Size = New System.Drawing.Size(309, 28)
+        Me.Txt2.TabIndex = 3
+        '
+        'BtnDividir
+        '
+        Me.BtnDividir.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnDividir.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnDividir.Location = New System.Drawing.Point(263, 239)
+        Me.BtnDividir.Name = "BtnDividir"
+        Me.BtnDividir.Size = New System.Drawing.Size(57, 50)
+        Me.BtnDividir.TabIndex = 4
+        Me.BtnDividir.Text = "/"
+        Me.BtnDividir.UseVisualStyleBackColor = False
+        '
+        'BtnPunto
+        '
+        Me.BtnPunto.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnPunto.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnPunto.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnPunto.Location = New System.Drawing.Point(202, 295)
+        Me.BtnPunto.Name = "BtnPunto"
+        Me.BtnPunto.Size = New System.Drawing.Size(57, 50)
+        Me.BtnPunto.TabIndex = 4
+        Me.BtnPunto.Text = "."
+        Me.BtnPunto.UseVisualStyleBackColor = False
+        '
+        'BtnIgual
+        '
+        Me.BtnIgual.BackColor = System.Drawing.Color.MidnightBlue
+        Me.BtnIgual.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnIgual.Font = New System.Drawing.Font("Segoe Print", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnIgual.Location = New System.Drawing.Point(263, 295)
+        Me.BtnIgual.Name = "BtnIgual"
+        Me.BtnIgual.Size = New System.Drawing.Size(57, 50)
+        Me.BtnIgual.TabIndex = 4
+        Me.BtnIgual.Text = "="
+        Me.BtnIgual.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkCyan
+        Me.ClientSize = New System.Drawing.Size(334, 371)
+        Me.Controls.Add(Me.Btn3)
+        Me.Controls.Add(Me.Btn6)
+        Me.Controls.Add(Me.BtnIgual)
+        Me.Controls.Add(Me.BtnPunto)
+        Me.Controls.Add(Me.BtnDividir)
+        Me.Controls.Add(Me.BtnMultiplicar)
+        Me.Controls.Add(Me.BtnResta)
+        Me.Controls.Add(Me.BtnSuma)
+        Me.Controls.Add(Me.BtnBorrar)
+        Me.Controls.Add(Me.Btn9)
+        Me.Controls.Add(Me.Btn2)
+        Me.Controls.Add(Me.Btn5)
+        Me.Controls.Add(Me.Btn8)
+        Me.Controls.Add(Me.Btn0)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.Btn4)
+        Me.Controls.Add(Me.Btn7)
+        Me.Controls.Add(Me.Txt2)
+        Me.Controls.Add(Me.Txt1)
+        Me.Controls.Add(Me.BtnCerrar)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnCerrar As Button
+    Friend WithEvents Txt1 As TextBox
+    Friend WithEvents Btn7 As Button
+    Friend WithEvents Btn8 As Button
+    Friend WithEvents Btn9 As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents Btn6 As Button
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents Btn0 As Button
+    Friend WithEvents BtnBorrar As Button
+    Friend WithEvents BtnSuma As Button
+    Friend WithEvents BtnResta As Button
+    Friend WithEvents BtnMultiplicar As Button
+    Friend WithEvents Txt2 As TextBox
+    Friend WithEvents BtnDividir As Button
+    Friend WithEvents BtnPunto As Button
+    Friend WithEvents BtnIgual As Button
+End Class
---
+++ Gonzales Almeyda/Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,172 @@
+﻿Public Class Form1
+    Private memoria1 As Double = 0.0
+    Private memoria2 As Double = 0.0
+    Private signo As String
+    Private Sub calcular()
+        Select Case signo
+            Case "+"
+                Txt1.Text = memoria1 + memoria2
+            Case "-"
+                Txt1.Text = memoria1 - memoria2
+            Case "*"
+                Txt1.Text = memoria1 * memoria2
+            Case "/"
+                Txt1.Text = memoria1 / memoria2
+            Case Else
+                MsgBox("Error")
+        End Select
+    End Sub
+    Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Txt1_TextChanged(sender As Object, e As EventArgs) Handles Txt1.TextChanged
+
+    End Sub
+
+    Private Sub Btn0_Click(sender As Object, e As EventArgs) Handles Btn0.Click
+        Txt1.Text = Txt1.Text & "0"
+    End Sub
+
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        Txt1.Text = Txt1.Text & "1"
+    End Sub
+
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        Txt1.Text = Txt1.Text & "2"
+    End Sub
+
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
+        Txt1.Text = Txt1.Text & "3"
+    End Sub
+
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+        Txt1.Text = Txt1.Text & "4"
+    End Sub
+
+    Private Sub Btn5_Click(sender As Object, e As EventArgs) Handles Btn5.Click
+        Txt1.Text = Txt1.Text & "5"
+    End Sub
+
+    Private Sub Btn6_Click(sender As Object, e As EventArgs) Handles Btn6.Click
+        Txt1.Text = Txt1.Text & "6"
+    End Sub
+
+    Private Sub Btn7_Click(sender As Object, e As EventArgs) Handles Btn7.Click
+        Txt1.Text = Txt1.Text & "7"
+    End Sub
+
+    Private Sub Btn8_Click(sender As Object, e As EventArgs) Handles Btn8.Click
+        Txt1.Text = Txt1.Text & "8"
+    End Sub
+
+    Private Sub Btn9_Click(sender As Object, e As EventArgs) Handles Btn9.Click
+        Txt1.Text = Txt1.Text & "9"
+    End Sub
+
+    Private Sub BtnBorrar_Click(sender As Object, e As EventArgs) Handles BtnBorrar.Click
+        Try
+            Dim largo As Integer
+            If Txt1.Text <> "" Then
+                largo = Txt1.Text.Length
+                Txt1.Text = Mid(Txt1.Text, 1, largo - 1)
+
+            End If
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnSuma_Click(sender As Object, e As EventArgs) Handles BtnSuma.Click
+        Try
+            If Txt1.Text <> "" Then
+                memoria1 = Val(Txt1.Text)
+                signo = "+"
+                Txt2.Clear()
+                Txt2.Text = Txt1.Text & "+"
+                Txt1.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnResta_Click(sender As Object, e As EventArgs) Handles BtnResta.Click
+        Try
+            If Txt1.Text <> "" Then
+                memoria1 = Val(Txt1.Text)
+                signo = "-"
+                Txt2.Clear()
+                Txt2.Text = Txt1.Text & "-"
+                Txt1.Clear()
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnIgual_Click(sender As Object, e As EventArgs) Handles BtnIgual.Click
+        Try
+            If Txt1.Text <> "" And memoria1 <> 0.0 Then
+                memoria2 = Txt1.Text
+                Txt2.Text = Txt2.Text & Txt1.Text & "="
+                calcular()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnMultiplicar_Click(sender As Object, e As EventArgs) Handles BtnMultiplicar.Click
+        Try
+            If Txt1.Text <> "" Then
+                memoria1 = Val(Txt1.Text)
+                signo = "*"
+                Txt2.Clear()
+                Txt2.Text = Txt1.Text & "x"
+                Txt1.Clear()
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub BtnDividir_Click(sender As Object, e As EventArgs) Handles BtnDividir.Click
+        Try
+            If Txt1.Text <> "" Then
+                memoria1 = Val(Txt1.Text)
+                signo = "/"
+                Txt2.Clear()
+                Txt2.Text = Txt1.Text & "/"
+                Txt1.Clear()
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Function existepunto(ByVal cadena As String) As Boolean
+        Dim largo As Integer
+        Dim respuesta As Boolean = False
+        largo = cadena.Length
+
+        For i As Integer = 1 To largo Step 1
+            If Mid(cadena, i, 1) = "." Then
+                respuesta = True
+            End If
+        Next
+
+        Return respuesta
+    End Function
+    Private Sub BtnPunto_Click(sender As Object, e As EventArgs) Handles BtnPunto.Click
+        If Txt1.Text = "" Then
+            Txt1.Text = "0."
+        ElseIf existepunto(Txt1.Text) = False Then
+            Txt1.Text = Txt1.Text & "."
+        End If
+    End Sub
+End Class
